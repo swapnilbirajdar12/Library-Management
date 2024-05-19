49db5c56f1685371a61946a9fa96a588d3567fd4
vivekgogi <114925625+vivekgogi@users.noreply.github.com>
2024-05-19 18:24:25 +0530

    Update Footer.js

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -14,9 +14,9 @@ function Footer() {
                     <div className="contact-details">
                         <h1>Contact Us</h1>
                         <p>Librarian</p>
-                        <p>Government School</p>
-                        <p>Visakhapatnam-530041</p>
-                        <p>Andhra Pradesh</p>
+                        <p>WIT College</p>
+                        <p>Solapur-413003</p>
+                        <p>Maharashtra</p>
                         <p>India</p>
                         <p><b>Email:</b>example@gmail.com</p>
                     </div>
@@ -26,26 +26,3 @@ function Footer() {
                         <a href='#home'>Link-1</a>
                         <a href='#home'>Link-1</a>
                         <a href='#home'>Link-1</a>
-                    </div>
-                    <div className='librarian-details'>
-                        <h1>Librarian</h1>
-                        <p>Name</p>
-                        <p>Education</p>
-                        <p>Contact: +91 9123456787</p>
-                    </div>
-                </div>
-                <div className="contact-social" >
-                    <a href='#home' className='social-icon'><TwitterIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
-                    <a href='#home' className='social-icon'><LinkedInIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
-                    <a href='#home' className='social-icon'><TelegramIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
-                    <a href='#home' className='social-icon'><InstagramIcon style={{ fontSize: 40,color:"rgb(283,83,75)"}} /></a>
-                </div>
-            </div>
-            <div className='copyright-details'>
-                <p className='footer-copyright'>&#169; 2020 copyright all right reserved<br /><span>Designed with ❤️ by Pranavdhar Reddy</span></p>
-            </div>
-        </div>
-    )
-}
-
-export default Footer
